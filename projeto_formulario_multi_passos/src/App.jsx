aa628829d416af2f0a104a2a924170ced9e1a195
William Monte de Moura <williamonte@gmail.com>
2023-04-04 12:02:12 -0300

    Exercício 01

---
+++ projeto_formulario_multi_passos/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import DadosPessoais from './components/DadosPessoais'
+import Endereco from './components/Endereco'
+import Agradecimento from './components/Agradecimento'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
